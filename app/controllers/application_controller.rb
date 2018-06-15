5f015d46dcf5218938ab70246eaa3db3a25f35bc
Taylor Campbell <taylor.campbell@rtslabs.com>
2018-06-15 17:14:48 -0400

    render things in rows

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
   def current_organization
-    current_user.organization
+    current_user&.organization
   end
   helper_method :current_organization
 end
