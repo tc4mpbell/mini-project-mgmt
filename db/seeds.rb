fd6c56197e615b135a7ba6fbb6c65d6d95b3089e
Taylor Campbell <taylor.campbell@rtslabs.com>
2018-06-15 16:22:38 -0400

    style

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,18 @@
 if Rails.env.development?
   require "#{Rails.root}/db/seeds/development.rb"
 end
+
+
+example_org = Organization.find_or_create_by(name: "My First Org")
+
+admin = User.find_or_initialize_by(
+  first_name: 'Jane', 
+  last_name: 'Smith', 
+  organization: example_org, 
+  email: 'jane@example.com').tap do |u|
+  u.password = "password"
+  u.save!
+end
+
+
+example_org.projects.find_or_create_by(name: 'First Project')
