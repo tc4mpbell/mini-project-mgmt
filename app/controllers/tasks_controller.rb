03b914e1d38beb9883c3631cc073f864452dae99
Taylor Campbell <taylor.campbell@rtslabs.com>
2018-06-15 23:49:47 -0400

    start updating tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  def update
+    # @TODO authorize
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      respond_to do |format|
+        format.js 
+      end
+    else
+      respond_to do |format|
+        format.js {
+          render 422
+        }
+      end
+    end
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task)
+  end
+end
